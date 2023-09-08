7eb75c5792700c05af285925dd96ef82af86cba1
Harshal-95 <96761542+Harshal-95@users.noreply.github.com>
2023-09-08 18:56:01 +0530

    Create create_db.sql

---
+++ create_db.sql
@@ -0,0 +1,16 @@
+-- Create a new SQLite database named students.db
+ATTACH DATABASE "students.db" AS students;
+
+-- Create the students table
+CREATE TABLE students.students (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    enrollment_number TEXT NOT NULL UNIQUE,
+    name TEXT NOT NULL,
+    department TEXT NOT NULL,
+    year_of_diploma INTEGER NOT NULL,
+    date_of_birth DATE NOT NULL,
+    contact_number TEXT NOT NULL,
+    address TEXT NOT NULL
+);
+
+
