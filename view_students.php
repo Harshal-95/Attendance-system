15fa61d2895e5a7d4558d97d6b5cf0465e727d17
Harshal-95 <96761542+Harshal-95@users.noreply.github.com>
2023-09-09 11:01:25 +0530

    view_students.php

--- view_students.php
+++ view_students.php
@@ -1,43 +1,37 @@
+<?php
+session_start();
+
+if (!isset($_SESSION["user"])) {
+    // User is not logged in, redirect to login page
+    header("Location: index.php");
+    exit;
+}
+
+// Database connection code (similar to previous examples)
+
+// Retrieve and display student details
+$query = $db->query("SELECT * FROM students");
+?>
 <!DOCTYPE html>
 <html>
 <head>
-    <title>View Registered Students</title>
+    <title>Student Details</title>
 </head>
 <body>
-    <h2>Registered Students</h2>
-
-    <?php
-    // Connect to the SQLite database
-    $db = new SQLite3("students.db");
-
-    // Query to retrieve all registered students
-    $query = $db->query("SELECT * FROM students");
-
-    if ($query) {
-        echo "<table border='1'>";
-        echo "<tr><th>ID</th><th>Name</th><th>Enrollment Number</th><th>Department</th><th>Year of Diploma</th><th>Date of Birth</th><th>Contact Number</th><th>Address</th></tr>";
-
-        while ($row = $query->fetchArray(SQLITE3_ASSOC)) {
-            echo "<tr>";
-            echo "<td>" . $row["id"] . "</td>";
-            echo "<td>" . $row["name"] . "</td>";
-            echo "<td>" . $row["enrollment_number"] . "</td>";
-            echo "<td>" . $row["department"] . "</td>";
-            echo "<td>" . $row["year_of_diploma"] . "</td>";
-            echo "<td>" . $row["date_of_birth"] . "</td>";
-            echo "<td>" . $row["contact_number"] . "</td>";
-            echo "<td>" . $row["address"] . "</td>";
-            echo "</tr>";
-        }
-
-        echo "</table>";
-    } else {
-        echo "Error: " . $db->lastErrorMsg();
-    }
-
-    // Close the database connection
-    $db->close();
-    ?>
-
+    <h2>Student Details</h2>
+    <table border="1">
+        <tr>
+            <th>Name</th>
+            <!-- Add other table headers here -->
+        </tr>
+        <?php while ($row = $query->fetchArray(SQLITE3_ASSOC)) { ?>
+            <tr>
+                <td><?php echo $row["name"]; ?></td>
+                <!-- Add other table cells here -->
+            </tr>
+        <?php } ?>
+    </table>
+    <a href="profile.php">Back to Profile</a><br>
+    <a href="logout.php">Logout</a>
 </body>
 </html>
